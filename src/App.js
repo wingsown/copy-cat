15da0627409f4c8930daf9820d99cdc09d5f2d3b
Wilson <wj.buena@gmail.com>
2021-10-26 11:05:58 +0800

    Added component instance to app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import { CopyCat } from './CopyCat';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-        Learn React
-        </p>
+      <header className="App-header"> 
+        <CopyCat/>
       </header>
     </div>
   );
