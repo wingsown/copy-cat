bf5b23a4f88179ffeaf5f3965943f725f162b46e
Wilson <wj.buena@gmail.com>
2021-10-26 11:01:20 +0800

    Added new component

---
+++ src/CopyCat.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const images = {
+    copycat: 'https://content.codecademy.com/courses/React/react_photo_copycat.png',
+    quietcat: 'https://content.codecademy.com/courses/React/react_photo_quietcat.png'
+}
