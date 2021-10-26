a4b67ce633b751d93a420743ff0807c5657ae210
Wilson <wj.buena@gmail.com>
2021-10-26 11:03:51 +0800

    Added jsx to CopyCat component

--- src/CopyCat.js
+++ src/CopyCat.js
@@ -4,3 +4,16 @@ const images = {
     copycat: 'https://content.codecademy.com/courses/React/react_photo_copycat.png',
     quietcat: 'https://content.codecademy.com/courses/React/react_photo_quietcat.png'
 }
+
+export class CopyCat extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Copy Cat</h1>
+                <input
+                type="text"/>
+                <img/>
+            </div>
+        )
+    }
+}
