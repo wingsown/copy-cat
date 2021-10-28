d83c66fc956603c86b4bef218234e59309343cff
Wilson <wj.buena@gmail.com>
2021-10-28 11:45:17 +0800

    Added constructor methods

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import './App.css';
 import { CopyCat } from './CopyCat';
 
 export class CopyCatContainer extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      copying: true,
+      input: ''
+    };
+
+    this.toggleTape = this.toggleTape.bind(this);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  toggleTape() {
+    this.setState({copying: !this.state.copying})
+  }
+
+  handleChange(e) {
+    this.setState({input: e.target.value})
+  }
+
   render() {
     return (
       <div className="App">
