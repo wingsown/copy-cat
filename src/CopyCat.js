4ac346eb358dc6002d07175cf909fdb812192135
Wilson <wj.buena@gmail.com>
2021-10-28 11:53:41 +0800

    Destructuring of props in CopyCat component

--- src/CopyCat.js
+++ src/CopyCat.js
@@ -8,6 +8,7 @@ const images = {
 
 export class CopyCat extends React.Component {
     render() {
+        const {copying, toggleTape, value, handleChange } = this.props;
         return (
             <div style={styles.divStyles}>
                 <h1 style={{marginBottom: 80}}>Copy Cat</h1>
@@ -15,6 +16,9 @@ export class CopyCat extends React.Component {
                 type="text"/>
                 <img
                 style={styles.imgStyles}
+                alt='cat'
+                src={copying ? images.copycat : images.quietcat}
+                onClick={toggleTape}
                 />
             </div>
         )
